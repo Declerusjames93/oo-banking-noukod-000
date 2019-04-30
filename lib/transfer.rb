d6dca12b7077a51bc76d8540c0380e3f03ebc336
James Declerus <james.declerus@esih.edu>
2019-04-30 18:12:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,39 +1,36 @@
 class Transfer
   # your code here
   attr_accessor :sender, :receiver, :amount, :status
-
-   def initialize(sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
-    @status = "pending"
-  end
-
-   def both_valid?
-    sender.valid? && receiver.valid? ? true : false
-  end
-
-   def execute_transaction
-    if both_valid? && status == "pending"
-      if amount < self.sender.balance
-        self.sender.balance -= amount
-        self.receiver.balance += amount
-        self.status = "complete"
-      else
-         self.status = "rejected"
-         "Transaction rejected. Please check your account balance."
-      end
-    else
+ 
+    def initialize(sender, receiver, amount, status="pending")
+     @sender = sender
+     @receiver = receiver
+     @amount = amount
+     @status = status
+   end
+ 
+    def valid?
+     sender.valid? && receiver.valid?
+   end
+ 
+    def execute_transaction
+     if valid? && sender.balance > amount && self.status == "pending"
+       sender.withdrawal(self.amount)
+       receiver.deposit(self.amount)
+       self.status = "complete"
+     else
        self.status = "rejected"
        "Transaction rejected. Please check your account balance."
-    end
-  end
-
-   def reverse_transfer
-    if self.status == "complete"
-      self.sender.balance += amount
-      self.receiver.balance -= amount
-      self.status = "reversed"
-    end
-  end
+     end
+   end
+ 
+    def reverse_transfer
+     if valid? && receiver.balance > amount && self.status == "complete"
+       sender.deposit(self.amount)
+       receiver.withdrawal(self.amount)
+       self.status = "reversed"
+     else
+       self.status = "rejected"
+       "Transaction rejected. Please check your account balance."      
+     end
 end
